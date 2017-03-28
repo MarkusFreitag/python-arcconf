c17644488ba57ee5aa0194f492bf40418291210b
Markus Freitag <fmarkus@mailbox.org>
2017-03-28 19:02:32 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Setup file to install the pyarcconf package."""
+from setuptools import setup
+
+setup(name='pyarcconf',
+      version='0.1.0',
+      description='Python3 library for the arrconf tool.',
+      author='Markus Freitag',
+      author_email='fmarkus@mailbox.org',
+      license='MIT',
+      packages=['pyarcconf'],
+      zip_safe=False)
