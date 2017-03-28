b66da770c6433c61a2fafbfbb6af86b9e4677c08
Markus Freitag <fmarkus@mailbox.org>
2017-03-28 19:12:32 +0200

    add method to autoconvert key, value pairs into more practical datatypes

--- pyarcconf/parser.py
+++ pyarcconf/parser.py
@@ -11,3 +11,35 @@ def cut_lines(info_str, start, end):
         str: cutted info_str
     """
     return '\n'.join(info_str.split('\n')[start:end*-1])
+
+
+def convert_attribute(key, value):
+    """Convert an attribute into the most pratical datatype.
+   
+    If the value is 'enabled', 'yes', 'true', 'disabled', 'no' or 'false'
+    it will be converted into boolean, otherwise it stays a string.
+    Args:
+        key (str): attribute key
+        value (str): attribute value
+    Returns:
+        str, bool: key, value pair
+        str, str: formated key, value pair
+    """
+    key = key.strip().lower()
+    for char in [' ', '-', ',', '/']:
+        key = key.replace(char, '_')
+    for char in ['.']:
+        key = key.replace(char, '')
+    if '(' in key:
+        key = key.split('(')[0]
+
+    if len(value.split()) == 2:
+        size, unit = value.split()
+        if size.isdigit() and unit in ['B', 'KB', 'MB', 'GB']:
+            return key, conv_bytes_up(size, unit)
+    value = value.strip().lower()
+    if value in ['enabled', 'yes', 'true']:
+        return key, True
+    elif value in ['disabled', 'no', 'false']:
+        return key, False
+    return key, value
